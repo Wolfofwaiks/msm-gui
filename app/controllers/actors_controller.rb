8a4314b48b44e994447009e51798282ea6258187
Wolfofwaiks <165302970+Wolfofwaiks@users.noreply.github.com>
2024-05-24 18:45:41 +0000

    trying something risky

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -39,4 +39,23 @@ class ActorsController < ApplicationController
 
     redirect_to('/actors')
   end
+
+  def update 
+    a_id = params.fetch("the_id")
+
+    matching_records = Actor.where({ :id=> m_id })
+
+    the_actor = matching_records.at(0)
+
+    the_actor.name = params.fetch("the_name")
+    the_actor.dob = params.fetch("the_dob")
+    the_actor.bio = params.fetch("the_bio")
+    the_actor.image = params.fetch("the_image")
+
+    the_actor.save
+
+    redirect_to("/actor/#{the_actor.id}")
+   
+
+  end
 end
